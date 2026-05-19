e5fec83b7dab22daa9cdba65a224b3119edc39be
Lucas TREIBER-LUSTIG <lucas.treiber-lustig@etu.unistra.fr>
2026-05-19 09:17:21 +0200

    Innitialisation de TurnLogic

--- src/gameLogic/TurnLogic.java
+++ src/gameLogic/TurnLogic.java
@@ -1,4 +1,35 @@
 package gameLogic;
 
+import actor.Player;
+import actorLogic.BotLogic;
+
 public class TurnLogic {
+    private GameBordLogic m_gamebord;
+    private StackLogic m_stack;
+
+    public TurnLogic(GameBordLogic gamebord, StackLogic stack){
+        m_gamebord = gamebord;
+        m_stack = stack;
+    }
+
+    public void botTurn(BotLogic bot){
+
+    }
+
+    public void playerTurn(PlayerLogic player){
+
+    }
+
+    public void placeCard(){
+
+    }
+
+    public void drawCard(ActorLogic actor){
+
+    }
+
+    public void resolveAttacks(PlayerLogic player, BotLogic bot){
+
+    }
+
 }
