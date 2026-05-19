e535fef9e86e2ae15f202393b28dc6d2b0ceef7f
Lucas TREIBER-LUSTIG <lucas.treiber-lustig@etu.unistra.fr>
2026-05-19 10:19:53 +0200

    Ajout resolveAttacks, drawCard et placeCard de TurnLogic

--- src/gameLogic/TurnLogic.java
+++ src/gameLogic/TurnLogic.java
@@ -1,7 +1,10 @@
 package gameLogic;
 
-import actor.Player;
+import actorLogic.PlayerLogic;
 import actorLogic.BotLogic;
+import actorLogic.ActorLogic;
+import cardLogic.AnimalLogic;
+import cardLogic.CardLogic;
 
 public class TurnLogic {
     private GameBordLogic m_gamebord;
@@ -20,16 +23,63 @@ public class TurnLogic {
 
     }
 
-    public void placeCard(){
-
+    //Place les cartes du joueur ou du bot sur leur ligne respectif
+    // (ne met pas les cartes du bot sur bot line se serra fait tout seul après resolveAttacks)
+    public void placeCard(ActorLogic actor, int IndexHand, int position){
+        if(actor instanceof PlayerLogic){
+            m_gamebord.setPlayerLine(actor.removeCard(IndexHand), position);
+        }else{
+            m_gamebord.setPreviewLine(actor.removeCard(IndexHand), position);
+        }
     }
 
     public void drawCard(ActorLogic actor){
-
+        actor.addCard(m_stack.drawCard());
     }
 
-    public void resolveAttacks(PlayerLogic player, BotLogic bot){
+    public int resolveAttacks(PlayerLogic player, BotLogic bot){
+        int score = 0;
+        for(int i = 0; i < 4; i++){
+            if(m_gamebord.getPlayerLine(i) != null && m_gamebord.getPlayerLine(i) instanceof AnimalLogic animal){
+                if(m_gamebord.getBotLine(i) != null) {
+                    score += animal.attack(player, m_gamebord.getBotLine(i));
+                } else {
+                    score += animal.attack(player,m_gamebord.getPreviewLine(i));
+                }
+            }
+        }
 
+        for(int i = 0 ; i < 4 ; i++){
+            if(m_gamebord.getBotLine(i) != null && m_gamebord.getBotLine(i).isDead()){
+                m_gamebord.removeBotLine(i);
+            }
+            if(m_gamebord.getPreviewLine(i) != null && m_gamebord.getPreviewLine(i).isDead()){
+                m_gamebord.removePreviewLine(i);
+            }
+        }
+
+        for(int i = 0 ; i < 4 ; i++){
+            if(m_gamebord.getPreviewLine(i) != null && m_gamebord.getBotLine(i) == null){
+                m_gamebord.setBotLine(m_gamebord.removePreviewLine(i), i);
+            }
+        }
+
+        for(int i = 0 ; i < 4 ; i++){
+            if(m_gamebord.getBotLine(i) != null && m_gamebord.getBotLine(i) instanceof AnimalLogic animal){
+                if(m_gamebord.getPlayerLine(i) != null) {
+                    score += animal.attack(player, m_gamebord.getBotLine(i));
+                } else {
+                    score += animal.attack(player,m_gamebord.getPreviewLine(i));
+                }
+            }
+        }
+
+        for(int i = 0 ; i < 4 ; i++) {
+            if(m_gamebord.getPlayerLine(i) != null && m_gamebord.getPlayerLine(i).isDead()){
+                m_gamebord.removePlayerLine(i);
+            }
+        }
+        return score;
     }
 
 }
