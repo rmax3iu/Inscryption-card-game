357e10a4363074738454887615c270406af7fa16
Maxime PEIGNIER <peignierm@gmail.com>
2026-05-19 10:20:48 +0200

    Ajout des classes RoundLogic, GameBordLogic, GameLogic

--- src/gameLogic/GameBordLogic.java
+++ src/gameLogic/GameBordLogic.java
@@ -2,17 +2,50 @@ package gameLogic;
 
 import cardLogic.CardLogic;
 
+import javax.smartcardio.Card;
+
 public class GameBordLogic {
 
     private CardLogic[] m_playerLine;
     private CardLogic[] m_botLine;
     private CardLogic[] m_previewLine;
 
-    public GameBordLogic(){
+    public GameBordLogic()
+    {
         m_playerLine = new CardLogic[4];
         m_botLine = new CardLogic[4];
         m_previewLine = new CardLogic[4];
     }
 
+    public void setPlayerLine(CardLogic card, int position)
+    {
+        this.m_playerLine[position] = card;
+    }
 
+    public CardLogic getPlayerLine(int position)
+    {
+        return this.m_playerLine[position];
+    }
+
+    public void setBotLine(CardLogic card, int position)
+    {
+        this.m_botLine[position] = card;
+    }
+
+    public CardLogic getBotLine(int position)
+    {
+        return this.m_botLine[position];
+    }
+
+    public void setPreviewLine(CardLogic card, int position)
+    {
+        this.m_previewLine[position] = card;
+    }
+
+    public CardLogic getPreviewLine(int position)
+    {
+        return this.m_previewLine[position];
+    }
+
+    // faire une methode remove pr chaqsue ligne
 }
--- src/gameLogic/RoundLogic.java
+++ src/gameLogic/RoundLogic.java
@@ -1,13 +1,71 @@
 package gameLogic;
 
+import actorLogic.BotLogic;
+import actorLogic.PlayerLogic;
+import cardLogic.CardLogic;
+
+import java.util.ArrayList;
+
 public class RoundLogic
 {
     private Integer m_score;
+    private GameBordLogic m_gamebord;
+    private StackLogic m_stack;
+    private PlayerLogic m_player;
+    private BotLogic m_bot;
 
-    public RoundLogic()
+    public RoundLogic(ArrayList<CardLogic> cards)
     {
-
+        this.m_score = 0;
+        this.m_gamebord = new GameBordLogic();
+        this.m_stack = new StackLogic(cards);
     }
 
-    public
+    public void play(PlayerLogic player, BotLogic bot)
+    {
+        this.m_player = player;
+        this.m_bot = bot;
+
+        while(!isEnd())
+        {
+            // 1. On instancie un nouveau tour en lui passant le plateau et la pioche
+            TurnLogic m_currentTurn = new TurnLogic(m_gamebord, m_stack);
+
+            // 2. Le joueur joue son tour
+            m_currentTurn.playerTurn(m_player);
+
+            // 3. Le bot joue son tour
+            m_currentTurn.botTurn(m_player);
+
+            // 4. On résout les attaques et on récupère les points de dégâts
+            int pointTurn;
+            pointTurn = m_currentTurn.resolveAttacks(m_player, m_bot);
+
+            // 5. On met à jour le score
+            updateScore(pointTurn);
+        }
+    }
+
+    public void updateScore(int newScore)
+    {
+        this.m_score += newScore;
+    }
+
+    public Boolean isWon()
+    {
+        if(this.m_score >= 5 || this.m_score <= -5)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean isEnd()
+    {
+        if(this.m_score> 0)
+        {
+            return true;
+        }
+        return false;
+    }
 }
