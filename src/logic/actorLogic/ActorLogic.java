5460cc85be3702c1af00960c4bcfe5e2374b5a22
Maxime PEIGNIER <peignierm@gmail.com>
2026-06-02 11:19:44 +0200

    Ajout des commentaires pour la classe ActorLogic

--- src/logic/actorLogic/ActorLogic.java
+++ src/logic/actorLogic/ActorLogic.java
@@ -7,59 +7,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ActorLogic {
-    //2 String pour le nom soit Player soit Bot
+    // On définit les deux types d'acteurs possibles pour le jeu : le joueur humain et le bot
     public static final String PLAYER = "Player";
     public static final String BOT = "Bot";
 
-    private List<CardLogic> m_hand;     //Liste de carte, c'est la main de l'acteur
-    private int m_bones;                //Le nombre d'os de l'acteur
+    // On déclare les attributs de l'acteur
+    private List<CardLogic> m_hand;
+    private int m_bones;
     private String m_name;
 
-    private ActorLogic(String name) {
+    private ActorLogic(String name)
+    {
         m_hand = new ArrayList<>();
         m_bones = 0;
         m_name = name;
     }
 
+    // On génère un nouvel acteur configuré spécifiquement comme un bot
     public static ActorLogic newBotLogic(){
         return new ActorLogic(BOT);
     }
 
+    // On génère un nouvel acteur configuré spécifiquement comme un joueur humain
     public static ActorLogic newPlayerLogic(){
         return new ActorLogic(PLAYER);
     }
 
-    //Renvoie le nom de l'acteur donc logiquement soit Player soit Bot si c'est bien fait
+    // On retourne le nom de l'acteur afin de savoir s'il s'agit du joueur ou du bot
     public String getName(){
         return m_name;
     }
 
-    //Ajoute une carte dans sa main
+    // On ajoute une nouvelle carte dans la main de l'acteur
     public void addCard(CardLogic card) {
         m_hand.add(card);
     }
 
-    //Retire et renvoie une carte de sa main à un indice donné
+    // On retire et on récupère la carte située à un indice précis dans la main
     public CardLogic removeCard(int index) {
         return m_hand.remove(index);
     }
 
-    //Renvoie juste une carte de sa main à un indice donné
+    // On consulte et on renvoie la carte de la main correspondant à l'indice demandé sans la supprimer
     public CardLogic getCard(int index) {
         return m_hand.get(index);
     }
 
-    //Renvoie le nombre de carte dans sa main
+    // On récupère le nombre total de cartes que l'acteur a actuellement en main
     public int handSize() {
         return m_hand.size();
     }
 
-    //Renvoie son nombre d'os
+    // On renvoie le nombre total d'os que l'acteur possède
     public int getBones() {
         return m_bones;
     }
 
-    //Incrément son nombre d'os
+    // On augmente le nombre d'os de l'acteur de la valeur reçue en paramètre
     public void addBones(int amount) {
         this.m_bones += amount;
     }
