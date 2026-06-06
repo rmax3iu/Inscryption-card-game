cc00f66a10f36fd2080c0a3d6debac045988d64b
Maxime PEIGNIER <peignierm@gmail.com>
2026-06-06 21:03:50 +0200

    Reformulation des commentaires et mise à la ligne des accolades dans CardFactory

--- src/logic/cardLogic/CardFactory.java
+++ src/logic/cardLogic/CardFactory.java
@@ -2,74 +2,89 @@ package logic.cardLogic;
 
 import logic.cardLogic.powers.*;
 
-
-public class CardFactory //Va créer les différents animaux
+// Sert à instancier les différents animaux et obstacles du jeu
+public class CardFactory
 {
     public static final int NB_CARD = 13;
 
-    // --- CARTES ANIMAUX TERRESTRES ---
+    // Les cartes animaux terrestres
 
-    public static AnimalLogic createChat() {
-        return new AnimalLogic("Chat", 1, 0, SummonCostLogic.newBloodCost(1), new ManyLife()); // name, hp, attack, cost, power(optionel)
+    public static AnimalLogic createChat()
+    {
+        return new AnimalLogic("Chat", 1, 0, SummonCostLogic.newBloodCost(1), new ManyLife());
     }
 
-    public static AnimalLogic createGrizzly() {
+    public static AnimalLogic createGrizzly()
+    {
         return new AnimalLogic("Grizzly", 6, 4, SummonCostLogic.newBloodCost(3));
     }
 
-    public static AnimalLogic createCoyote() {
+    public static AnimalLogic createCoyote()
+    {
         return new AnimalLogic("Coyote", 1, 2, SummonCostLogic.newBonesCost(4));
     }
 
-    public static AnimalLogic createEcureuil() {
+    public static AnimalLogic createEcureuil()
+    {
         return new AnimalLogic("Ecureuil", 1, 0, SummonCostLogic.newFree());
     }
 
-    public static AnimalLogic createHermine() {
+    public static AnimalLogic createHermine()
+    {
         return new AnimalLogic("Hermine", 3, 1, SummonCostLogic.newBloodCost(1));
     }
 
-    public static AnimalLogic createLouveteau() {
+    public static AnimalLogic createLouveteau()
+    {
         return new AnimalLogic("Louveteau", 1, 1, SummonCostLogic.newBloodCost(1), new Growth());
     }
 
-    public static AnimalLogic createLoup() {
+    public static AnimalLogic createLoup()
+    {
         return new AnimalLogic("Loup", 2, 3, SummonCostLogic.newBloodCost(2));
     }
 
-    public static AnimalLogic createPunaise() {
+    public static AnimalLogic createPunaise()
+    {
         return new AnimalLogic("Punaise", 2, 1, SummonCostLogic.newBonesCost(2), new Stinking());
     }
 
-    public static AnimalLogic createElan() {
+    public static AnimalLogic createElan()
+    {
         return new AnimalLogic("Elan", 4, 2, SummonCostLogic.newBloodCost(2), new Runner());
     }
 
-    public static AnimalLogic createVipere() {
+    public static AnimalLogic createVipere()
+    {
         return new AnimalLogic("Vipère", 1, 1, SummonCostLogic.newBloodCost(2), new DeadlyContact());
     }
 
-    public static AnimalLogic createPorcEpic() {
+    public static AnimalLogic createPorcEpic()
+    {
         return new AnimalLogic("Porc-épic", 2, 1, SummonCostLogic.newBloodCost(1), new SharpSpikes());
     }
 
-    // --- CARTES ANIMAUX VOLANTS ---
+    // Les cartes animaux volants
 
-    public static FlyingLogic createMoineau() {
+    public static FlyingLogic createMoineau()
+    {
         return new FlyingLogic("Moineau", 2, 1, SummonCostLogic.newBloodCost(1));
     }
 
-    public static FlyingLogic createCorbeau() {
+    public static FlyingLogic createCorbeau()
+    {
         return new FlyingLogic("Corbeau", 3, 2, SummonCostLogic.newBloodCost(2));
     }
 
-    // --- CARTES OBSTACLES ---
+    // Les cartes obstacles
 
-    public static CardLogic createRocher() {
+    public static CardLogic createRocher()
+    {
         return new CardLogic("Rocher", 5);
     }
 
-    public static CardLogic createSapin() {
+    public static CardLogic createSapin()
+    {
         return new CardLogic("Sapin", 3);
     }
 }
