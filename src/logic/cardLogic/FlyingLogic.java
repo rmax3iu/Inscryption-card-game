092c8fb1a40d7549993366fc42b4d7867e380121
Maxime PEIGNIER <peignierm@gmail.com>
2026-06-06 21:10:31 +0200

    Ajout de la méthode toString() et reformulation des commentaires dans FlyingLogic

--- src/logic/cardLogic/FlyingLogic.java
+++ src/logic/cardLogic/FlyingLogic.java
@@ -4,27 +4,46 @@ import logic.cardLogic.powers.Power;
 
 import java.util.Optional;
 
-public class FlyingLogic extends AnimalLogic {
-
-    public FlyingLogic(String name, int hp, int attack, SummonCostLogic cost) {
+public class FlyingLogic extends AnimalLogic
+{
+    public FlyingLogic(String name, int hp, int attack, SummonCostLogic cost)
+    {
         super(name, hp, attack, cost);
     }
 
-    public FlyingLogic(String name, int hp, int attack, SummonCostLogic cost, Power power) {
+    public FlyingLogic(String name, int hp, int attack, SummonCostLogic cost, Power power)
+    {
         super(name, hp, attack, cost, power);
     }
 
+    // Les cartes volantes attaquent directement le score et elles ignorent la carte adverse sauf si elle a le pouvoir stiking
     @Override
-    public int attack(Optional<CardLogic> target) {
-        // Les volantes attaquent directement le score, elles ignorent la carte adverse sauf si elle a le pouvoir stiking
+    public int attack(Optional<CardLogic> target)
+    {
         int att = getAttack();
-        if(target.isPresent() && target.get().hasPower()){
+        if (target.isPresent() && target.get().hasPower())
+        {
             Optional<Power> power = target.get().getPower();
-            if (power.isPresent()){
+            if (power.isPresent())
+            {
                 att -= power.get().attackModifierOnFacing();
             }
         }
         return att;
     }
 
+    @Override
+    public String toString()
+    {
+        String nomPouvoir;
+        if (getPower().isPresent())
+        {
+            nomPouvoir = getPower().get().getName();
+        }
+        else
+        {
+            nomPouvoir = "Aucun";
+        }
+        return "Animal volant : " + getName() + " | Attaque : " + getAttack() + " | PV : " + getHp() + " | Pouvoir : " + nomPouvoir + " | Coût : " + getSummonCost();
+    }
 }
