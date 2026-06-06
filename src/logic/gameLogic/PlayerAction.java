64030be25e57ddd7f2e32e881ce6c5fc10663c0d
Maxime PEIGNIER <peignierm@gmail.com>
2026-06-06 21:46:15 +0200

    Correction des commentaires dans PlayerAction

--- src/logic/gameLogic/PlayerAction.java
+++ src/logic/gameLogic/PlayerAction.java
@@ -7,46 +7,66 @@ import logic.cardLogic.CardLogic;
 
 import java.util.Optional;
 
-public class PlayerAction {
-    public PlayerAction() {}
+public class PlayerAction
+{
+    public PlayerAction()
+    {
+    }
 
-    public void drawCard(ActorLogic player, StackLogic stack) {
-        if(!stack.isEmpty()) {
+    public void drawCard(ActorLogic player, StackLogic stack)
+    {
+        if (!stack.isEmpty())
+        {
             AnimalLogic card = stack.draw();
             player.addCard(card);
             Message.tell("Tu as pioché une carte " + card.getName() + ".");
         }
     }
 
-    public boolean validateCost(ActorLogic player, GameBoardLogic board, AnimalLogic card) {
-        if(card.getSummonCost().isBonesCost() && player.getBones() >= card.getSummonCost().getBones()){
+    public boolean validateCost(ActorLogic player, GameBoardLogic board, AnimalLogic card)
+    {
+        if (card.getSummonCost().isBonesCost() && player.getBones() >= card.getSummonCost().getBones())
+        {
             return true;
-        }else if(card.getSummonCost().isBloodCost() && board.countPlayerCards() >= card.getSummonCost().getBlood()){
+        }
+        else if (card.getSummonCost().isBloodCost() && board.countPlayerCards() >= card.getSummonCost().getBlood())
+        {
             return true;
-        }else if(card.getSummonCost().isFree()){
+        }
+        else if (card.getSummonCost().isFree())
+        {
             return true;
-        }else{
+        }
+        else
+        {
             return false;
         }
     }
 
-    public void payCost(ActorLogic player, GameBoardLogic board, AnimalLogic card) {
-        if(card.getSummonCost().isBloodCost()){
-            sacrificePlayerCards(board,card.getSummonCost().getBlood());
+    public void payCost(ActorLogic player, GameBoardLogic board, AnimalLogic card)
+    {
+        if (card.getSummonCost().isBloodCost())
+        {
+            sacrificePlayerCards(board, card.getSummonCost().getBlood());
             player.addBones(card.getSummonCost().getBlood());
-        }else if (card.getSummonCost().isBonesCost()){
+        }
+        else if (card.getSummonCost().isBonesCost())
+        {
             player.addBones(-card.getSummonCost().getBones());
         }
     }
 
-    public void sacrificePlayerCards(GameBoardLogic board, int count) {
+    public void sacrificePlayerCards(GameBoardLogic board, int count)
+    {
         int nbSacrifie = 0;
-        while(nbSacrifie < count) {
-            //On demande la carte qu'on veut sacrifier
+        while (nbSacrifie < count)
+        {
+            // On demande la carte qu'on veut sacrifier
             String reponse = Message.ask("Donne la position de la carte que vous voulez sacrifier (il reste " + (count - nbSacrifie) + " cartes à sacrifier)");
             int index;
-            //On traduit la demande en un indice pour connaitre la carte
-            switch (reponse) {
+            // On traduit la demande en un indice pour connaître la carte
+            switch (reponse)
+            {
                 case "B1":
                     index = 0;
                     break;
@@ -60,21 +80,25 @@ public class PlayerAction {
                     index = 3;
                     break;
                 default:
-                    index = -1;     //Quand le joueur écrit qu'elle que chose d'inattendue
+                    index = -1;     // Quand le joueur écrit quelque chose d'inattendu
                     break;
             }
 
-            //On vérifie que l'utilisateur demande à sacrifier une carte sur une case du plateau qui existe
-            if (index != -1) {
+            // On vérifie que l'utilisateur demande à sacrifier une carte sur une case du plateau qui existe
+            if (index != -1)
+            {
                 Optional<CardLogic> card = board.getPlayerLine(index);
 
-                //On vérifie que la carte peut être sacrifié (en gros si c'est un obstacle ou une vraie carte)
-                if (card.isPresent() && card.get().canBeSacrify()) {
-                    board.setPlayerLine(index, card.get().sacrify());       //On sacrifie la carte et le retour de sacrify() c'est soit un Optional.empty soit la même carte c'est pour le pouvoir plusieurs vies(Many life)
+                // On vérifie que la carte peut être sacrifiée (en gros si c'est un obstacle ou une vraie carte)
+                if (card.isPresent() && card.get().canBeSacrify())
+                {
+                    board.setPlayerLine(index, card.get().sacrify());       // On sacrifie la carte et le retour de sacrify() c'est soit un Optional.empty soit la même carte c'est pour le pouvoir plusieurs vies(Many life)
                     nbSacrifie++;
                 }
-            }else{
-                Message.tell("Saisi incorrect ! (type de saisie valide : B1/B2/B3/B4)");
+            }
+            else
+            {
+                Message.tell("Saisie incorrecte ! (type de saisie valide : B1/B2/B3/B4)");
             }
         }
     }
